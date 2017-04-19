a973627701dc9840e44606758e7444781f7bbc6f
xSzymo <szymekssss@wp.pl>
2017-04-19 21:26:28 +0200

    0.1 - configuration

---
+++ src/main/java/xszymo/configuration/MyWebInitializer.java
@@ -0,0 +1,23 @@
+package xszymo.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MyWebInitializer extends
+		AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { SpringWebConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+}
